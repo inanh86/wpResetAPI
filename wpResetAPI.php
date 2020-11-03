34f3965084097224093d29313502f933c593d427
inanh86 <pos@inanh86.com>
2020-11-03 10:46:08 +0700

    Update

--- wpResetAPI.php
+++ wpResetAPI.php
@@ -1,4 +1,28 @@
-<?php 
-if(!defined('ABSPATH')) {
-    exit;
+<?php
+/**
+ * Plugin Name: inanh86.com - ResetAPI Wordpress
+ * Plugin URI: https://inanh86.com/ResetAPI
+ * Description: Tùy Chỉnh là cổng Reset API Wordpress
+ * Version: 0.1
+ * Author: inanh86.com
+ * Author URI: https://inanh86.com/ResetAPI
+ * Text Domain: inanh86-resetAPI
+ */
+defined( 'ABSPATH' ) || exit;
+
+// Define POS_PLUGIN_FILE.
+if ( ! defined( 'API_PLUGIN_FILE' ) ) {
+	define( 'API_PLUGIN_FILE', __FILE__ );
 }
+if ( !defined( "API_PLUGIN_DIR_PATH" ) ) {
+	define( "API_PLUGIN_DIR_PATH", plugins_url('' , __FILE__) );
+}
+// Include the main WooCommerce class.
+if ( ! class_exists( 'Hero_POS', false ) ) {
+	include_once dirname( API_PLUGIN_FILE ). '/src/class-pos.php';
+}
+function POS86() {
+	return Hero_POS::instance();
+}
+// Global for backwards compatibility.
+$GLOBALS['pos'] = POS86();
