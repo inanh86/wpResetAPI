cccc32f6ffe59bd6a6739908d30c4614c9d494ba
inanh86 <pos@inanh86.com>
2021-03-19 09:47:24 +0700

    New úp

--- src/db/class-customer.php
+++ src/db/class-customer.php
@@ -1,6 +1,7 @@
 <?php namespace inanh86\DB;
 
 use inanh86\DB\DB;
+use Exception;
 
 if (!defined('ABSPATH')) {
     exit;
@@ -18,8 +19,21 @@ class Khachhang extends DB {
 
     public function __construct($query=null) {
         if( !empty($query) ) {
-            
+            $this->query($query);
         }
+    }
+    /**
+     * Gọi danh sách khách hàng theo 
+     * @param [] $data
+     */
+    public function query($data=null) {
+
+    }
+    /**
+     * Trả kết quả khi xử lý thành công
+     */
+    public function results() {
+        return $this->results;
     }
     /**
      * Đăng nhập Client
@@ -27,12 +41,19 @@ class Khachhang extends DB {
      * @param string $pass
      */
     public function dang_nhap($id=null,$pass=null) {
-        if (!empty($id) && !empty($pass)) {
-            $this->data['id'] = sanitize_text_field($id);
-            $this->data['pass'] = sanitize_text_field($pass);
-            return $this->data['id'];
+        
+        $this->data['id'] = sanitize_text_field($id);
+        $this->data['pass'] = sanitize_text_field($pass);
+        $client = $this->goi_khach_hang('login', $this->data['id']);
+
+        if( $client && wp_check_password( $this->data['pass'], $client[0]->pass) ) {
+            return $this->results = [
+                'id' => $client[0]->khach_hang_id,
+                'phone' => $client[0]->phone,
+                'name' => $this->goi_meta($client[0]->khach_hang_id, 'ten_khach_hang'),
+            ];
         } else {
-            throw new \Exception("Số điện thoại và mật khẩu của bạn không được để trống", self::LOGIN_FAIL);
+            throw new Exception("Mật khẩu của bạn không đúng", self::LOGIN_FAIL);
         }
     }
     /**
@@ -48,9 +69,14 @@ class Khachhang extends DB {
         $this->data['diachi'] = sanitize_text_field($diachi);
         $set_date = date('Y-m-d H:i:s');
         
+        // Kiểm tra phone number
+        if ( is_phone_number($this->data['id']) ) {
+            throw new Exception("Số điện thoại khách hàng bị sai, hình như thiếu số", self::FAIL_PROSS);
+        }
+
         // kiểm tra sdt khách hàng trước khi khởi tạo
-        if( $this->goi_khach_hang($this->data['id'],'phone') ) {
-            throw new \Exception("Khách hàng đã tồn tại", self::FAIL_PROSS);
+        if( $this->goi_khach_hang( 'phone', $this->data['id'] ) ) {
+            throw new Exception("Khách hàng đã tồn tại", self::FAIL_PROSS);
         }
 
         // Thêm Khách hàng vào database
@@ -63,45 +89,80 @@ class Khachhang extends DB {
                 $set_date, // cài đặt ngày khởi tạo khách hàng
             ]
         ));
+        
+        // Thêm db meta cho customer
+        $this->add_data([
+            'id' => $wpdb->insert_id,
+            'meta_value' => $meta->value,
+            'meta_key' => $meta->key
+        ]);
+        
 
         // Báo lổi nếu không insert được
         if( !$new_khachang ) {
-            throw new \Exception("Có lổi trong quá trình xử lý tạo khách hàng mới", self::FAIL_PROSS);   
+            throw new Exception("Có lổi trong quá trình xử lý tạo khách hàng mới", self::FAIL_PROSS);   
         }
 
         // Trả kết quả khi hoàn thành 
         return $this->resouce('Thêm khách hàng thành công');
     }
     /**
-     * Gọi data khách hàng || kiểm tra id khách hàng || trả thông tin khách hàng
+     * Gọi data khách hàng theo id 
+     * 
+     * @@ Sử dụng để gọi 1 khách hàng duy nhất
      * @param int $id sử dụng id khách hàng để gọi
      * @use id hoặc số điện thoại
      */
-    public function goi_khach_hang($id=null,$phone=null) {
+    public function goi_khach_hang($type=null, $id=null) {
         global $wpdb;
 
         if( empty($id) ) {
-            throw new \Exception("id khách hàng không tồn tại", self::FAIL_PROSS);
+            throw new Exception("id khách hàng không tồn tại", self::FAIL_PROSS);
         }
-        if ( !strlen($id) === 9 ) {
-            throw new \Exception("Lổi số điện thoại khách hàng không đúng", self::FAIL_PROSS);   
+        if ( is_phone_number($id) ) {
+            throw new Exception("Lổi số điện thoại khách hàng không đúng", self::FAIL_PROSS);   
         }
-        // kiểm tra đầu vào là id hay là sdt 
-        if(!empty($phone)) {
-            $this->data['id'] = substr($id, 1,8);
+        if( $type === "phone" || $type === 'login' ) {
+            $id_khachHang = substr($id, 1,8);
         } else {
-            $this->data['id'] = $id;
+            $id_khachHang = $id;
         }
-        
+
         // xử lý query truy cập kiểm tra xem khách hàng có mặt ở đó k?
         $query = $wpdb->get_results($wpdb->prepare(
-            "SELECT * FROM {$wpdb->prefix}{$this->khachhang} WHERE login = %d", [$this->data['id']]
+            "SELECT * FROM {$wpdb->prefix}{$this->khachhang} WHERE login = %d", [$id_khachHang]
         ));
 
         if( !$query ) {
             return false;
         }
+        return $query;
+    }
+    /**
+     * Thêm data cho khách hàng
+     * @param int $id
+     */
+    public function add_data($data=null) {
+        global $wpdb;
+
+        // Xử lý query db
+        $meta_khachhang = $wpdb->query($wpdb->prepare(
+            "INSERT INTO {$wpdb->prefix}khach_hang_meta (khach_hang_id, meta_key, meta_value) VALUES(%d, %s, %s)",
+            [$data['id'], $data['meta_key'], $data['meta_value']]
+        ));
+
+        // Trả về lổi nếu có 
+        if(!$meta_khachhang) {
+            throw new \Exception("Lổi không thể thêm được thông tin của khách hàng", self::FAIL_PROSS);
+        }
+        return $meta_khachhang;
+    }
+    /**
+     * gọi thông tin thêm của khách trong bảng meta
+     * @param
+     */
+    public function goi_meta($id=null, $meta_key=null) {
+        global $wpdb;
         
-        return $this->resouce($query);
     }
 }
--- src/function-cores.php
+++ src/function-cores.php
@@ -6,3 +6,10 @@ function docFile($filename) {
     $file = file_get_contents($file);
     return json_decode($file);
 }
+/**
+ * Kiểm tra đây có phải là số điện thoại hày không
+ * @param string $phone
+ */
+function is_phone_number($phone) {
+    return preg_match('/^[0-9]{9}+$/', $phone);
+}
